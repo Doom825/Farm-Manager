bb1db683e2e3a604f8e08e8a104c09af2c00c486
IvelisBecker <ivelisbecker@gmail.com>
2025-04-02 21:00:44 -0500

    Added navbar

---
+++ client/src/components/NavTabs.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// Bootstrap css
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+// https://react-bootstrap.netlify.app/docs/components/navbar/ 
+export default function NavTabs() {
+    return (
+      <>
+        <Navbar bg="dark" data-bs-theme="dark" fixed="top">
+          <Container> 
+            <Link to='/'>
+              <Navbar.Brand><h1>Farm Management</h1></Navbar.Brand>
+            </Link>
+            <Nav className="me-auto">
+              <Link to='/'>
+                Home
+              </Link>
+              <Link to='/Farm'>
+                My Farm
+              </Link>
+              <Link to='/Weather'>
+                Weather
+              </Link>
+              <Link to='/LogIn'>
+                Log In
+              </Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </>
+    );
+  }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome to Your Farm</h1>
+            <h2>Manage your farm in one place</h2>
+        </div>
+    );
+}
