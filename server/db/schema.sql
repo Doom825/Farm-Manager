de3c7b020471c0e6bd49edfda35ec956cfd0830a
Ctbambrose <ctbambrose@gmail.com>
2025-04-03 21:50:08 -0500

    update schema.sql and added seeds.sql

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -4,21 +4,30 @@ CREATE DATABASE user_db;
 \c user_db;
 
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  user_name VARCHAR(25) NOT NULL
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL
 );
 
--- CREATE TABLE crop (
---   id SERIAL PRIMARY KEY,
---   user_id INT,
---   crop VARCHAR
--- );
+CREATE TABLE crops (
+    crop_id SERIAL PRIMARY KEY,
+    crop_name VARCHAR(255) UNIQUE NOT NULL
+);
 
--- CREATE TABLE cropJournal (
---   id SERIAL PRIMARY KEY,
---   title VARCHAR,
---   body TEXT,
---   user_id INT,
---   crop_id INT,
---   entry_date timestamp
--- );
+CREATE TABLE user_crop (
+    user_id INT,
+    crop_id INT,
+    PRIMARY KEY (user_id, crop_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (crop_id) REFERENCES crops(crop_id) ON DELETE CASCADE
+);
+
+CREATE TABLE cropJournal (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR,
+  body TEXT,
+  user_id INT,
+  crop_id INT,
+  entry_date timestamp,
+  CONSTRAINT FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE SET NULL,
+  CONSTRAINT FOREIGN KEY (crop_id) REFERENCES crops(id) ON DELETE SET NULL
+);
---
+++ server/db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO users (user_name)
+VALUES ('ctbambrose'),
+       ('xX_Farmer_Xx'),
+       ('jedfalk'),
+       ('Doom825'),
+       ('I-0110');
+
+-- INSERT INTO crops (user_id, crop_name)
+-- VALUES (1, 'Zazu is underrated. Give that hornbill a sequel!'),
+--        (2, 'I''m gonna make him an offer you can''t refuse, watch this movie'),
+--        (1, 'Scar is the lion everyone loves to hate'),
+--        (3, 'Ten years of ballet and three years of tap to join a gang in this neighborhood'),
+--        (5, 'The tin man gave a metallic, hollow performance'),
+--        (1, 'Hakuna matata'),
+--        (5, 'Those flying monkeys are nightmare fuel!');
+       
