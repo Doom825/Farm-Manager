55347703125b469e286c0406ac64bbbc7c2d3547
IvelisBecker <ivelisbecker@gmail.com>
2025-04-05 09:22:40 -0500

    Broken auth setup.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,12 @@
-// Flag to control whether to force a database refresh on server start
-
+// Import express to run server app
 import express from 'express';
+// Import these packages to create auth
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
 
 const app = express();  // Create an Express application
 const PORT = process.env.PORT || 3001;  // Define the port for the server to listen on
@@ -8,8 +14,14 @@ const PORT = process.env.PORT || 3001;  // Define the port for the server to lis
 // Serves static files from the client's dist folder, typically for a built React application
 app.use(express.static('../client/dist'));
 
-// app.use(express.json());  // Middleware to parse JSON request bodies
-// // app.use(routes);  // Use the imported routes for handling API endpoints
+app.use(express.json());  // Middleware to parse JSON request bodies
+
+app.use(cors()); // Allows you to control which origins are allowed to access your resources. More info. https://www.npmjs.com/package/cors 
+
+app.use(routes);  // Use the imported routes for handling API endpoints
+
+const 
+
 app.get('/', (_req, res) => {
   res.send("Welcome to the Farm Management from the Backend side... the dark side!")
 });
