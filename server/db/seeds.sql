a9b10f638a53b609aac89107afa9a271e3faa8a6
Ctbambrose <ctbambrose@gmail.com>
2025-04-09 18:34:35 -0500

    Updated the seeds.sql

--- server/db/seeds.sql
+++ server/db/seeds.sql
@@ -5,21 +5,14 @@ VALUES ('ctbambrose', 'TestPass', 'test1@test.com'),
        ('Doom825', 'TestPass', 'test4@test.com'),
        ('I-0110', 'TestPass', 'test5@test.com');
 
-INSERT INTO crops (crop_name, scientific_name)
-VALUES ('Tomato'),
-       ('Cabbage'),
-       ('Carrot'),
-       ('Corn'),
-       ('Potato');
-
 INSERT INTO user_crop (user_id, crop_id)
 VALUES (1, 1),
-       (1, 2),
-       (2, 3),
-       (2, 4),
-       (2, 5),
-       (3, 4),
-       (4, 1),
+       (1, 56),
+       (2, 13),
+       (2, 20),
+       (2, 84),
+       (3, 45),
+       (4, 7),
        (4, 2),
        (5, 5),
        (5, 1);
